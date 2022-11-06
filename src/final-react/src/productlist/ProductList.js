4aa4506ccccc56f5426dd85c80a27afa10a05f6a
Jaewoo Park <jaewoo7868@gmail.com>
2022-11-06 16:10:53 +0900

    [update] 1. 상품 리스트 출력 2. mapper, controller , sql 추가

--- src/final-react/src/productlist/ProductList.js
+++ src/final-react/src/productlist/ProductList.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 function ProductList(props) {
-    return (
-        <div>
-            <h1>상품 리스트 폼</h1>
-        </div>
-    );
+  localStorage.url = process.env.REACT_APP_URL;
+  const navi = useNavigate();
+  const [productlist, setProductlist] = useState();
+
+  const { currentPage } = useParams();
+  console.log("proCP:" + currentPage);
+
+  const productUrl = localStorage.url + "/product/";
+  console.log("proUrl:" + productUrl);
+
+  const getPageList = () => {
+    let url = localStorage.url + "/product/list?currentPage=" + currentPage;
+
+    console.log("Url:" + url);
+    axios.get(url).then((res) => {
+      console.log("axios 성공");
+      setProductlist(res.data);
+    });
+  };
+
+  useEffect(() => {
+    console.log("list 호출");
+    getPageList();
+  }, [currentPage]);
+
+  return (
+    <div>
+      <h1>상품 리스트 폼</h1>
+      {productlist &&
+        productlist.list.map((pl, i) => (
+          <Link to={`/product/detail/${pl.p_num}`} key={i}>
+            <div
+              style={{
+                width: "300px",
+                height: "420px",
+                display: "inline-block",
+                marginLeft: "20px",
+                marginRight: "20px",
+                textAlign: "center",
+              }}
+              key={i}
+              p_num={pl.p_num}
+            >
+              <img
+                alt=""
+                src={productUrl + pl.photo}
+                width="300px"
+                height="300px"
+                style={{ margin: "0 auto" }}
+              />
+              <br />
+              <span>{pl.brand}</span>
+              <br />
+              <span>{pl.p_name}</span>
+              <br />
+              <span>{pl.price}</span>
+              <br />
+              <span>{pl.discount}</span>
+            </div>
+          </Link>
+        ))}
+    </div>
+  );
 }
 
 export default ProductList;
