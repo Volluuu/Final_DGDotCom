2db34345c96eb2827b4f5a1ecfae8b4eaf40ad18
DESKTOP-I4DAFN3\ny247 <ny247@naver.com>
2022-11-07 17:11:39 +0900

    리스트 레이아웃 수정중

--- src/final-react/src/productlist/ProductList.js
+++ src/final-react/src/productlist/ProductList.js
@@ -1,8 +1,38 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import {InputBase} from "@material-ui/core";
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import SearchIcon from '@material-ui/icons/Search';
+import Chip from '@material-ui/core/Chip';
+
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
 
 function ProductList(props) {
+
+
+    //필터
+    const [chipData, setChipData] = React.useState([
+        { key: 0, label: 'Angular' },
+        { key: 1, label: 'jQuery' },
+        { key: 2, label: 'Polymer' },
+        { key: 3, label: 'React' },
+        { key: 4, label: 'Vue.js' },
+    ]);
+
+    const handleDelete = (chipToDelete) => () => {
+        setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
+    };
+
+
   localStorage.url = process.env.REACT_APP_URL;
   const navi = useNavigate();
   const [productlist, setProductlist] = useState();
@@ -29,8 +59,48 @@ function ProductList(props) {
   }, [currentPage]);
 
   return (
-    <div>
-      <h1>상품 리스트 폼</h1>
+      <div>
+        <h3 style={{textAlign:'center'}} >SHOP</h3><br/>
+          <form style={{textAlign:'center', width:'700px', height:'150px',
+                        marginLeft:'400px'}}>
+            <TextField id="standard-basic" placeholder="검색어 입력"
+                       InputProps={{
+                         startAdornment: (
+                             <InputAdornment position="start">
+                               <SearchIcon />
+                             </InputAdornment>
+                         ),
+                       }}
+            />
+          </form>
+
+          <div style={{width:'15%', height:'700px',float:'left',
+                        marginLeft:'10px'}}>
+              <b>카테고리</b><br/><br/>
+              <Accordion>
+                  <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+                      <Typography >아우터</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                      <Typography>
+                          <FormControlLabel control={<Checkbox/>} label="자켓"/>
+                          <FormControlLabel control={<Checkbox/>} label="후드"/>
+                          <FormControlLabel control={<Checkbox/>} label="스웨트셔츠"/>
+                          <FormControlLabel control={<Checkbox/>} label="니트웨어"/>
+                          <FormControlLabel control={<Checkbox/>} label="긴팔 티셔츠"/>
+                          <FormControlLabel control={<Checkbox/>} label="반팔 티셔츠"/>
+                      </Typography>
+                  </AccordionDetails>
+              </Accordion>
+          </div>
+
+
+          <div style={{marginTop:'50px',marginLeft:'300px',width:'1000px',height:'100px'}}>
+                <Chip variant="outlined" color="primary" size="small" onDelete={handleDelete} label="테스트" />
+        </div>
+
+
+          <div style={{marginLeft:'300px',marginTop:'100px', width: '1050px'}}>
       {productlist &&
         productlist.list.map((pl, i) => (
           <Link to={`/product/detail/${pl.p_num}`} key={i}>
@@ -65,6 +135,7 @@ function ProductList(props) {
           </Link>
         ))}
     </div>
+      </div>
   );
 }
 
