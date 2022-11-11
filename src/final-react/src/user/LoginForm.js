1849beb2457f45c3468107dc9ab8e5954ac928c4
dglee95 <donggeonlee95@naver.com>
2022-11-11 17:07:25 +0900

    Home - 로그인해야 세션 주기 가능 menu - 로그인 / 로그아웃 시큐리티 로그아웃 구현 중

--- src/final-react/src/user/LoginForm.js
+++ src/final-react/src/user/LoginForm.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import "./UserCss.css";
 import axios from "axios";
@@ -14,46 +14,30 @@ function LoginForm(props) {
 
     const navi = useNavigate();
 
-    //submit 이벤트
-    // const onSubmitLogin = (e) => {
-    //     e.preventDefault();
-    //
-    //     // let url=sessionStorage.url+"/login/check";
-    //     let url = localStorage.url + "/user/login/check";
-    //
-    //
-    //     axios.post(url, {email, pass})
-    //         .then(res => {
-    //             console.log(res.data.check);
-    //             console.log(res.data.u_name);
-    //
-    //             // localstorage는 닫아도 남아있음
-    //             // sessionStorage은 닫으면 안남아있음
-    //
-    //             if (res.data.check === 1) {
-    //                 //각종정보를 Storage 에 저장
-    //                 // localStorage.loginok='yes';
-    //                 // localStorage.myid=myid;
-    //                 // localStorage.myname=res.data.myname;
-    //
-    //                 sessionStorage.loginok = 'yes';
-    //                 sessionStorage.email = email;
-    //                 sessionStorage.u_name = res.data.u_name;
-    //
-    //                 //일단 Home 으로 이동
-    //                 navi("/");
-    //                 window.location.reload();//새로고침
-    //             } else {
-    //                 alert("아이디 또는 비밀번호가 맞지 않습니다");
-    //                 setEmail('');
-    //                 setPass('');
-    //
-    //             }
-    //         });
-    // }
     const onSubmitLogin = (e) => {
         e.preventDefault();
-        // 이메일이 공백이거나 에러가 true 일 때 리턴
+        let signinUrl = process.env.REACT_APP_URL + "/user/signin";
+
+        axios.post(signinUrl, {email, pass})
+            .then(res => {
+                if (res.data === 0) {
+                    alert("아이디를 다시 확인해주세요.");
+                    setEmail('');
+                    setPass('');
+                    document.getElementById("email").focus();
+                    sessionStorage.removeItem("loginok");
+                } else if (res.data === -1) {
+                    alert("비밀번호가 틀렸습니다.");
+                    setPass('');
+                    document.getElementById("pass").focus();
+                    sessionStorage.removeItem("loginok");
+                } else {
+                    alert("로그인 성공");
+                    navi(-1);
+                    sessionStorage.u_num = res.data;
+                    sessionStorage.loginok = "yes";
+                }
+            })
     }
 
     const onLoginBtnState = () => {
@@ -66,21 +50,23 @@ function LoginForm(props) {
         }
     }
 
-    const loginBtnEvent = () => {
-        let signinUrl = process.env.REACT_APP_URL + "/user/signin";
-
-        axios.post(signinUrl, {email, pass})
-            .then(res => {
-                if (res.data === 0) {
-                    alert("아이디나 비밀번호가 틀렸습니다.");
-                    window.location.reload();
-                } else {
-                    alert("로그인 성공");
-                    window.location.reload();
-                    sessionStorage.u_num = res.data;
-                }
-            })
-    }
+    // const loginBtnEvent = () => {
+    //     let signinUrl = process.env.REACT_APP_URL + "/user/signin";
+    //
+    //     axios.post(signinUrl, {email, pass})
+    //         .then(res => {
+    //             if (res.data === 0) {
+    //                 alert("아이디나 비밀번호가 틀렸습니다.");
+    //                 window.location.reload();
+    //                 sessionStorage.loginok = "no";
+    //             } else {
+    //                 alert("로그인 성공");
+    //                 window.location.reload();
+    //                 sessionStorage.u_num = res.data;
+    //                 sessionStorage.loginok = "yes";
+    //             }
+    //         })
+    // }
 
     // email, pass 바뀔 때마다 렌더링
     useEffect(() => {
@@ -91,53 +77,55 @@ function LoginForm(props) {
         <div className="content lg" data-v-b02d33c2="">
             <div className="login_area" data-v-b02d33c2="">
                 <h2 style={{textAlign: "center"}}><span>DG.com Login</span></h2>
-                <div data-v-6c561060="" data-v-b02d33c2=""
-                     className={emailError ? "input_box has_error" : "input_box"}>
-                    <h3 className="input_title" data-v-6c561060="" data-v-b02d33c2="">이메일 주소</h3>
-                    <div className="input_item" data-v-6c561060="">
-                        <input type="text" placeholder="예) email@email.com" autoComplete="off" className="input_txt"
-                               value={email} data-v-6c561060=""
-                               onChange={(e) => {
-                                   setEmail(e.target.value);
-                                   let exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
-                                   if (exptext.test(email) === false) {
-                                       setEmailError(true);
-                                   } else {
-                                       setEmailError(false);
-                                   }
-                                   onLoginBtnState();
-                               }}/>
-                        {/*<button type="button" className="btn input_delete" style={{display: ""}} data-v-3d1bcc82=""*/}
-                        {/*        data-v-b02d33c2="" data-v-6c561060="">*/}
-                        {/*    <svg xmlns="http://www.w3.org/2000/svg" className="ico-delete-circle icon sprite-icons"*/}
-                        {/*         data-v-b02d33c2="">*/}
-                        {/*        <use href="/_nuxt/a9c19cb959b9bb0e43f56db79ee357b4.svg#i-ico-delete-circle"*/}
-                        {/*             data-v-b02d33c2=""></use>*/}
-                        {/*    </svg>*/}
-                        {/*</button>*/}
+                <form onSubmit={onSubmitLogin}>
+                    <div data-v-6c561060="" data-v-b02d33c2=""
+                         className={emailError ? "input_box has_error" : "input_box"}>
+                        <h3 className="input_title" data-v-6c561060="" data-v-b02d33c2="">이메일 주소</h3>
+                        <div className="input_item" data-v-6c561060="">
+                            <input type="text" placeholder="예) email@email.com" autoComplete="off" className="input_txt"
+                                   value={email} data-v-6c561060="" id="email"
+                                   onChange={(e) => {
+                                       setEmail(e.target.value);
+                                       let exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+                                       if (exptext.test(email) === false) {
+                                           setEmailError(true);
+                                       } else {
+                                           setEmailError(false);
+                                       }
+                                       onLoginBtnState();
+                                   }}/>
+                            {/*<button type="button" className="btn input_delete" style={{display: ""}} data-v-3d1bcc82=""*/}
+                            {/*        data-v-b02d33c2="" data-v-6c561060="">*/}
+                            {/*    <svg xmlns="http://www.w3.org/2000/svg" className="ico-delete-circle icon sprite-icons"*/}
+                            {/*         data-v-b02d33c2="">*/}
+                            {/*        <use href="/_nuxt/a9c19cb959b9bb0e43f56db79ee357b4.svg#i-ico-delete-circle"*/}
+                            {/*             data-v-b02d33c2=""></use>*/}
+                            {/*    </svg>*/}
+                            {/*</button>*/}
+                        </div>
+                        <p className="input_error" data-v-6c561060="" data-v-b02d33c2="">{emailErrorMsg}</p>
                     </div>
-                    <p className="input_error" data-v-6c561060="" data-v-b02d33c2="">{emailErrorMsg}</p>
-                </div>
-                <div data-v-6c561060="" data-v-b02d33c2=""
-                     className="input_box">
-                    <h3 className="input_title" data-v-6c561060="" data-v-b02d33c2="">비밀번호</h3>
-                    <div className="input_item" data-v-6c561060="">
-                        <input type="password" placeholder="" autoComplete="off" className="input_txt"
-                               data-v-6c561060="" value={pass}
-                               onChange={(e) => {
-                                   setPass(e.target.value);
-                                   onLoginBtnState();
-                               }}/></div>
-                </div>
-                <div className="login_btn_box" data-v-b02d33c2="">
-                    <button type={"submit"} className="btn full solid"
-                            disabled={loginBtn ? true : false}
-                            data-v-3d1bcc82=""
-                            data-v-b02d33c2=""
-                            onClick={loginBtnEvent}>
-                        로그인
-                    </button>
-                </div>
+                    <div data-v-6c561060="" data-v-b02d33c2=""
+                         className="input_box">
+                        <h3 className="input_title" data-v-6c561060="" data-v-b02d33c2="">비밀번호</h3>
+                        <div className="input_item" data-v-6c561060="">
+                            <input type="password" placeholder="" autoComplete="off" className="input_txt"
+                                   data-v-6c561060="" value={pass} id="pass"
+                                   onChange={(e) => {
+                                       setPass(e.target.value);
+                                       onLoginBtnState();
+                                   }}/></div>
+                    </div>
+                    <div className="login_btn_box" data-v-b02d33c2="">
+                        <button type={"submit"} className="btn full solid"
+                                disabled={loginBtn ? true : false}
+                                data-v-3d1bcc82=""
+                                data-v-b02d33c2=""
+                        >
+                            로그인
+                        </button>
+                    </div>
+                </form>
                 <ul className="look_box" data-v-b02d33c2="">
                     <li className="look_list" data-v-b02d33c2="">
                         <Link to="/join" className="look_link" data-v-b02d33c2=""> 이메일 가입 </Link>
