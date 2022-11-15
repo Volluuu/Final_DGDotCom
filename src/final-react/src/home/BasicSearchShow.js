a66f6c1ae5101b0bcd0bf3d8c7ecec278cf38034
movekim <sth9057@gmail.com>
2022-11-15 15:35:09 +0900

    메인페이지 swiper 적용 / footer 생성 / 메인 페이지 및 검색창 반응형 디자인 만드는중

--- src/final-react/src/home/BasicSearchShow.js
+++ src/final-react/src/home/BasicSearchShow.js
@@ -38,7 +38,16 @@ const CategoryElt = styled(Link)`
     padding-top: 5px;
   }
 `
-
+const PopLink = styled(Link)`
+    width: 130px;
+  height : 80px;
+  margin : 0 auto;
+  background-color: black;
+  color: white;
+  text-align: center;
+  line-height: 80px;
+  font-size: 18px;
+`
 const BasicSearchShow = () => {
     const [hot, setHot] = useState([]);
     const [hot_brand, setHot_brand] = useState([]);
@@ -58,7 +67,7 @@ const BasicSearchShow = () => {
     }, []);
 
     return (
-        <div style={{width: "768px", height: "800px"}}>
+        <div style={{width: "700px", height: "100%"}}>
             <Title_p>최근 검색어</Title_p>
             {
                 // user table에서 search 컬럼을 가져와서 list.map 구현
@@ -69,13 +78,13 @@ const BasicSearchShow = () => {
                 <Title_p style={{marginBottom: "5px"}}>추천 검색어</Title_p>
                 {
                     // Link로 돌리면 될듯 ㅋㅋ ez
-                    recommend.map((elt, idx) => <RecLink key={idx} to={`/product/list/1`}>{elt}</RecLink>)
+                    recommend.map((elt, idx) => <RecLink key={idx} to={`/product/list/?keyword=${elt}`}>{elt}</RecLink>)
                 }
             </div>
             <Title_p>인기 검색어</Title_p>
-            <div style={{marginBottom: "20px", width: "768px", display: "flex", justifyContent: "space-around"}}>
+            <div style={{marginBottom: "20px", width: "700px", display: "flex", justifyContent: "space-around"}}>
                 {/*//인기 검색어 테이블을 만들어야 할듯 없으면 테이블에 추가, 있으면 해당 검색어의 count를 1추가*/}
-                <ul style={{width: "384px", justifyContent: "left"}}>
+                <ul style={{width: "350px", justifyContent: "left"}}>
                     {
                         hot.filter((elt, idx) => idx < 5).map((elt, idx) =>
                             <li style={{marginBottom: "20px"}} key={idx}><HotLink key={idx}
@@ -84,11 +93,11 @@ const BasicSearchShow = () => {
                             </li>)
                     }
                 </ul>
-                <ul style={{width: "384px", justifyContent: "left"}}>
+                <ul style={{width: "350px", justifyContent: "left"}}>
                     {
                         hot.filter((elt, idx) => idx >= 5).map((elt, idx) =>
                             <li style={{marginBottom: "20px"}} key={idx}><HotLink key={idx}
-                                                                                  to={`/product/list/1?currentPage=1&keyword=${elt.word}`}><b style={{marginRight:"5px"}}>{idx + 6}</b> {elt.word}
+                                                                                  to={`/product/list/?currentPage=1&keyword=${elt.word}`}><b style={{marginRight:"5px"}}>{idx + 6}</b> {elt.word}
                             </HotLink>
                             </li>)
                     }
@@ -97,29 +106,28 @@ const BasicSearchShow = () => {
             <Title_p>카테고리</Title_p>
 
             <div style={{display:"flex" , marginBottom:"20px"}}>
-                <CategoryElt to={"/product/list/1"}>
+                <CategoryElt to={`/product/list/?currentPage=1&keyword=후드`}>
                     <img className={"CategoryEltPhoto"} src="http://localhost:9003/product/20221104020517860.PNG" alt=""/>
                     <p className={"CategoryEltInfo"}>상의</p>
                 </CategoryElt>
-                <CategoryElt to={"/product/list/1"}>
+                <CategoryElt to={`/product/list/?currentPage=1&keyword=바지`}>
                     <img className={"CategoryEltPhoto"} src="http://localhost:9003/product/20221104020634615.PNG" alt=""/>
                     <p className={"CategoryEltInfo"}>하의</p>
-                </CategoryElt><CategoryElt to={"/product/list/1"}>
+                </CategoryElt><CategoryElt to={`/product/list/?currentPage=1&keyword=패딩`}>
                 <img className={"CategoryEltPhoto"} src="http://localhost:9003/product/20221104001835856.PNG" alt=""/>
                 <p className={"CategoryEltInfo"}>아우터</p>
-            </CategoryElt><CategoryElt to={"/product/list/1"}>
+            </CategoryElt><CategoryElt to={`/product/list/?currentPage=1&keyword=패션잡화`}>
                 <img className={"CategoryEltPhoto"} src="http://localhost:9003/product/20221104022610637.PNG" alt=""/>
                 <p className={"CategoryEltInfo"}>패션잡화</p>
             </CategoryElt>
             </div>
 
             <Title_p>인기 브랜드</Title_p>
-            <div style={{width:"768px", backgroundColor:"skyblue", height:"200px", display:"flex", margin:"0 auto", flexWrap:"wrap"}}>
+            <div style={{width:"700px", height:"180px", display:"flex", flexWrap:"wrap"}}>
                 {
-                    hot_brand.map((elt, idx) => <Link key={idx} to={"/product/list/1"}>{elt.brand}</Link>)
+                    hot_brand.map((elt, idx) => <PopLink key={idx} to={`product/list/?currentPage=1&brands=${elt.brand}`}>{elt.brand}</PopLink>)
                 }
             </div>
-
         </div>
     );
 };
--- src/final-react/src/home/SearchModal.js
+++ src/final-react/src/home/SearchModal.js
@@ -8,6 +8,7 @@ import axios from "axios";
 import SearchCard from "./SearchCard";
 import SearchListVirtual from "./SearchListVirtual";
 import BasicSearchShow from "./BasicSearchShow";
+import styled from "styled-components";
 
 const useStyles = makeStyles((theme) => ({
     modal: {
@@ -20,6 +21,7 @@ const useStyles = makeStyles((theme) => ({
         border         : '2px solid #000',
         boxShadow      : theme.shadows[5],
         padding        : theme.spacing(2, 4, 3),
+        display:'flex',
     },
 }));
 
@@ -49,33 +51,41 @@ export default function TransitionsModal() {
     const onSubmit = useCallback(
         (e) => {
             e.preventDefault();
-            setOpen(false);
-            setWord('');
-            navigate("/product/list/");
+            if (word === '') {
+                navigate("/product/list");
+
+            } else {
+                navigate(`/product/list?keyword=${word}`)
+            }
+            const setOpenAndWord = () => {
+                setOpen(false);
+                setWord('');
+            }
         },
-        [],
+        [word],
     );
     const updateWord = () => {
-        const res = axios.get(`http://localhost:9003/list/search?word=${word}`).then(res=>{
+        const res = axios.get(`http://localhost:9003/list/search?word=${word}`).then(res => {
             setSearchList(res.data);
         })
     }
 
     useEffect(() => {
-        console.log("render!");
-            const debounce = setTimeout(()=>{
-                if(word){updateWord();} else{
-                    setSearchList([]);
-                }
-            },200)
-            return () => {
-                clearTimeout(debounce);
+        const debounce = setTimeout(() => {
+            if (word) {
+                updateWord();
+            } else {
+                setSearchList([]);
             }
+        }, 200)
+        return () => {
+            clearTimeout(debounce);
+        }
     }, [word]);
     return (
         <>
             <button type="button" onClick={handleOpen}
-                    style={{width: "50px", height: "30px", border: "3px solid black"}}>
+                    style={{width: "60px", height: "30px", border: "3px solid black"}}>
                 검색
             </button>
             <Modal
@@ -91,26 +101,36 @@ export default function TransitionsModal() {
                 }}
             >
                 <Fade in={open}>
-                    <div className={classes.paper}>
-                        <h2 id="transition-modal-title">찾으시는 상품의 상품명을 입력해주세요</h2>
+                    <div className={classes.paper} style={{height: "100%", display:"flex"}}>
                         <p id="transition-modal-description">
                             <form onSubmit={onSubmit}>
-                                <div style={{width:'768px', display:"flex", justifyContent:"space-between"}}>
-                                <input type="text" placeholder={"상품명 입력"} style={{
-                                    border     : "1px solid #ccc",
-                                    width      : "90%",
-                                    height     : "34px",
-                                    marginRight: "20px",
-                                }} name={"word"} value={word} onChange={changeWord}/>
-                                <button type={"submit"}
-                                        style={{backgroundColor: "black", color: "white", padding: "5px"}}>검색
-                                </button>
-                                </div>
-
+                                <InputBar>
+                                    <input className={"in"} type="text" placeholder={"상품명 입력"} autoFocus={true} style={{
+                                        border     : "1px solid #ccc",
+                                        width      : "90%",
+                                        height     : "34px",
+                                        marginRight: "20px",
+                                        marginTop  : "20px",
+                                    }} name={"word"} value={word} onChange={changeWord}/>
+                                    <button type={"submit"}
+                                            style={{
+                                                backgroundColor: "black",
+                                                color          : "white",
+                                                padding        : "5px",
+                                                marginTop      : "20px",
+                                                height        : "34px",
+                                                width: "50px",
+                                            }}>검색
+                                    </button>
+                                </InputBar>
+                                <div style={{margin:"0 auto"}}>
                                 {
-                                    word === '' ? <BasicSearchShow/> : searchList.length < 1 ? <div style={{width: "768px", height: "600px"}}>no result</div> : <SearchListVirtual searchlist={searchList}/>
+                                    word === '' ? <BasicSearchShow/> : searchList.length < 1 ?
+                                        <NoResult>no result</NoResult> :
+                                        <SearchListVirtual searchlist={searchList}/>
                                 }
-                                <NavLink to={"/product/list/1"} onClick={handleClose}>리스트로 이동</NavLink><br/>
+                                </div>
+                                <NavLink to={"/product/list"} onClick={handleClose}>상품 리스트로 이동</NavLink><br/>
                             </form>
                         </p>
                     </div>
@@ -119,3 +139,22 @@ export default function TransitionsModal() {
         </>
     );
 }
+
+const InputBar = styled.div`
+  width         : 700px;
+  display       : flex;
+  justify-content: space-between;
+  @media(max-width: 768px) {
+    width:432px;
+    & > input {
+      width: auto;
+    }
+  }
+`
+const NoResult = styled.div`
+    width:700px;
+  height:800px;
+  @media(max-width: 768px) {
+    width: 432px;
+  }
+`
